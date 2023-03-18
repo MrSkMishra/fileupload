58b4cc4fd76ee5752e0ebe1203c9776fb212c1b9
Sonu Kumar Mishra <mishrasonu9211@gmail.com>
2023-03-18 18:26:29 +0530

    first commit

---
+++ home/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render
+import shutil
+from tkinter import E
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.parsers import MultiPartParser
+from .serializers import *
+
+
+
+def home(request):
+    return render(request, 'home.html') 
+
+def download(request,uid):
+    context = {
+        "uid":uid,
+    }
+    return render(request,'download.html',context)
+
+
+
+class  HandleFileUpload(APIView):
+
+    def post(self,request):
+        try:
+            data = request.data
+
+            serializer = FileListSerializer(data = data)
+
+            if serializer.is_valid():
+                serializer.save()
+                return Response({
+                    'status':200,
+                    'message':'files uploaded successfully',
+                    'data':serializer.data
+                })
+            return Response({
+                'status':400,
+                'message':'something went wrong',
+                'data':serializer.errors
+            })
+        except Exception as e:
+            print(e)
+
